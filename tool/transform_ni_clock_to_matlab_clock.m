203a10384ec92586cbed95cd57a5cce8c2f1ff58
nfagan <nick@fagan.org>
2024-02-26 17:31:21 -0500

    use constant sample rate for clock

--- tool/transform_ni_clock_to_matlab_clock.m
+++ tool/transform_ni_clock_to_matlab_clock.m
@@ -22,27 +22,51 @@ vid_t_offset = vid_ts - clock_t0;
 sec_offset = seconds( vid_t_offset );
 
 ni_t = nan( numel(ni_sync_channel), 1 );
-for i = 1:numel(sync_ts)-1
-  i0 = sync_ts(i);
-  i1 = sync_ts(i + 1);
-  sync_t0 = sec_offset(i);
-  sync_t1 = sec_offset(i + 1);
+
+if ( 1 )
+  % Prefer (and assume) a constant sample rate.
+  sync_t0 = sec_offset(1);
+  i0 = sync_ts(1);
+  i1 = sync_ts(end);
+  sync_t1 = sync_t0 + (i1 - i0) / FS;
   num_points = i1 - i0 + 1;
   sync_t = linspace( sync_t0, sync_t1, num_points );
   ni_t(i0:i1) = sync_t;
-end
-
-if ( extrapolate )
-  % assign (approximate) timestamps to periods of the NI data that precede
-  % (pre) the first synch pulse and follow (post) the last. these 
-  % timestamps assume a constant sample rate of `FS`.
-  num_pre = sync_ts(1);
-  extrap_pre = sec_offset(1) - (0:num_pre-1)/FS;
-  ni_t(1:sync_ts(1)) = fliplr( extrap_pre );  
   
-  num_post = numel( ni_sync_channel ) - sync_ts(end) + 1;
-  extrap_post = sec_offset(end) + (0:num_post-1)/FS;
-  ni_t(sync_ts(end):end) = extrap_post;
+  if ( extrapolate )
+    num_pre = sync_ts(1);
+    extrap_pre = sec_offset(1) - (0:num_pre-1)/FS;
+    ni_t(1:sync_ts(1)) = fliplr( extrap_pre );  
+    
+    num_post = numel( ni_sync_channel ) - i1 + 1;
+    extrap_post = ni_t(i1) + (0:num_post-1)/FS;
+    ni_t(i1:end) = extrap_post;
+  end
+else
+  for i = 1:numel(sync_ts)-1  
+    i0 = sync_ts(i);
+    i1 = sync_ts(i + 1);
+    sync_t0 = sec_offset(i);
+    sync_t1 = sec_offset(i + 1);
+
+    num_points = i1 - i0 + 1;
+    sync_t = linspace( sync_t0, sync_t1, num_points );
+    ni_t(i0:i1) = sync_t;
+  end
+
+  if ( extrapolate )
+    % assign (approximate) timestamps to periods of the NI data that precede
+    % (pre) the first synch pulse and follow (post) the last. these 
+    % timestamps assume a constant sample rate of `FS`.
+    num_pre = sync_ts(1);
+    extrap_pre = sec_offset(1) - (0:num_pre-1)/FS;
+    ni_t(1:sync_ts(1)) = fliplr( extrap_pre );  
+
+    num_post = numel( ni_sync_channel ) - sync_ts(end) + 1;
+    extrap_post = sec_offset(end) + (0:num_post-1)/FS;
+    ni_t(sync_ts(end):end) = extrap_post;
+  end
+  
 end
 
 end
